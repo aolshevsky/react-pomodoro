11cf305ec114fc31f077797ac92aadf75d714919
Unknown <campapp.games@gmail.com>
2019-02-11 13:12:03 +0300

    Add Modal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Pomodoro from "./components/Pomodoro";
+import Modal from "./components/Settings";
 import "./App.css";
 
-const App = props => (
-  <div className="app">
-    <Pomodoro />
-  </div>
-);
+class App extends React.Component {
+  state = { showModal: false };
+  handleShowMessageClick = () => this.setState({ showModal: true });
+  handleCloseModal = () => this.setState({ showModal: false });
+  render() {
+    return (
+      <div className="app">
+        <button className="btn" onClick={this.handleShowMessageClick}>
+          Settings
+        </button>
+        {this.state.showModal ? (
+          <Modal onClose={this.handleCloseModal}>
+            This is the Pomodoro settings!
+          </Modal>
+        ) : null}
+        <Pomodoro />
+      </div>
+    );
+  }
+}
 
 export default App;
