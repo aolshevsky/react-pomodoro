9263151396fe9f7adacf8d2591fc282121404e50
Unknown <campapp.games@gmail.com>
2019-02-11 23:14:35 +0300

    Add action types

---
+++ src/components/Actions/index.jsx
@@ -0,0 +1,10 @@
+/*
+ action types
+*/
+
+export const SET_WORK_TIME = "SET_WORK_TIME";
+export const SET_RELAX_TIME = "SET_RELAX_TIME";
+export const SET_COFFEE_TIME = "SET_COFFEE_TIME";
+export const SET_LONG_BREAK = "SET_LONG_BREAK";
+export const SET_CHECK_NOTIFICATIONS = "SET_CHECK_NOTIFICATIONS";
+export const SET_CHECK_SOUNDS = "SET_CHECK_SOUNDS";
