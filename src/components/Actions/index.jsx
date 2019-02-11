d5bc0f11922cc06e44a6d7f86e0040361f0620f9
Unknown <campapp.games@gmail.com>
2019-02-11 23:39:19 +0300

    Add time types

--- src/components/Actions/index.jsx
+++ src/components/Actions/index.jsx
@@ -8,3 +8,11 @@ export const SET_COFFEE_TIME = "SET_COFFEE_TIME";
 export const SET_LONG_BREAK = "SET_LONG_BREAK";
 export const SET_CHECK_NOTIFICATIONS = "SET_CHECK_NOTIFICATIONS";
 export const SET_CHECK_SOUNDS = "SET_CHECK_SOUNDS";
+
+/*
+time types
+*/
+
+export const WORK_TIME = "work";
+export const RELAX_TIME = "relax";
+export const COFFEE_TIME = "coffee";
